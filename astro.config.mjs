35a1aba66019febb336a628135c00b9412d51c5b
Chidubem <emekaogbuchidubem@gmail.com>
2026-01-26 12:27:04 +0100

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 
+import sitemap from '@astrojs/sitemap';
+
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chidubem.name.ng',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
